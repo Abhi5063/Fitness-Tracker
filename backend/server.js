4856c8a85a96012f04e84b16acfe3a4e1e94fad7
Abhi5063 <abhiupadhyay5063@gmail.com>
2026-02-06 23:28:03 +0530

    Fix MongoDB connection environment variable

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Hello to Fitness Tracker API");
 });
 mongoose.connect(
-  process.env.MONGO_URI,
+  process.env.MONGODB_URI || process.env.MONGO_URI,
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
   () => console.log("Database connected")
 );
